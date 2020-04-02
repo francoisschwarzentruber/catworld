0a62f16440b0c2d1f2042fe0ebc6c928a9f2671d
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-04-03 00:33:18 +0200

    almost done :)

--- src/Cat.ts
+++ src/Cat.ts
@@ -2,30 +2,34 @@ type Vector2D = { x: number, y: number };
 
 
 const SPEEDDECREASING = 0.8;
-const MOVEMENT = 10;
+const MOVEMENT = 5;
 const GRAVITY = 10;
-const SIZE = 64;
-const DT = 0.2;
-const JUMP = 50;
+const SIZE = 32;
+const DT = 0.1;
+const JUMP = 10;
 
 export class Cat {
 
     public position: Vector2D;
     public speed: Vector2D;
     public accel: Vector2D;
-    private img = new Image();
+    private img;
+    private imgLeft = new Image();
+    private imgRight = new Image();
     private imgJump = new Image();
 
     constructor() {
-        this.position = { x: 100, y: 1000 };
+        this.position = { x: 350, y: 100 };
         this.speed = { x: 0, y: 0 };
         this.accel = { x: 0, y: 0 };
-        this.img.src = "./cat.png";
+        this.img = this.imgRight;
+        this.imgLeft.src = "./cat_left.png";
+        this.imgRight.src = "./cat_right.png";
         this.imgJump.src = "./catjump.png";
     }
 
-    left() { this.accel.x -= MOVEMENT; this.position.x--; }
-    right() { this.accel.x += MOVEMENT; this.position.x++; }
+    left() { this.img = this.imgLeft; this.accel.x -= MOVEMENT; this.position.x--; }
+    right() { this.img = this.imgRight; this.accel.x += MOVEMENT; this.position.x++; }
     jump() { this.accel.y = -JUMP; }
 
 
@@ -43,9 +47,9 @@ export class Cat {
     }
 
     draw(context: CanvasRenderingContext2D) {
-        if (this.accel.y < 0)
-            context.drawImage(this.img, this.position.x, this.position.y - SIZE, SIZE, SIZE);
+        if (this.accel.y > 0)
+            context.drawImage(this.img, this.position.x, this.position.y-SIZE, SIZE, SIZE);
         else
-            context.drawImage(this.imgJump, this.position.x, this.position.y - SIZE, SIZE, SIZE);
+            context.drawImage(this.imgJump, this.position.x, this.position.y-SIZE, SIZE, SIZE);
     }
 }
--- src/Game.ts
+++ src/Game.ts
@@ -6,44 +6,72 @@ export class Game {
     private canvas: HTMLCanvasElement;
     private dede: Cat;
     private scene: Scene;
+    private imgBackground = new Image();
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.scene = new Scene();
         this.dede = new Cat();
+
+        this.imgBackground.src = "./fond_coeur.png";
     }
 
     draw() {
-
-        let previousPosition = { x: this.dede.position.x, y: this.dede.position.y };
-
         for (let t = 0; t < 5; t++) {
+            let previousPosition = { x: this.dede.position.x, y: this.dede.position.y };
+
+            let score = this.scene.getGoodPositionScore(this.dede);
+
+
             this.dede.live();
 
-            let i = 0;
-            while(!this.scene.isGoodPosition(this.dede) ||( i <= 3)) {
-                this.dede.position.x -= this.dede.speed.x;
-                this.dede.position.y -= this.dede.speed.y;
-                i++;
+            if (this.scene.getGoodPositionScore(this.dede) < score) {
+                score = this.scene.getGoodPositionScore(this.dede);
+                this.dede.position.y-=10;
+                if (this.scene.getGoodPositionScore(this.dede) < score)
+                    this.dede.position = previousPosition;
+                else {
+                    this.dede.accel.y = 0;
+                    this.dede.speed.y = 0;
+                }
+                    
             }
-            if (i > 3) {
-                this.dede.position = previousPosition;
-                this.dede.accel = {x: -this.dede.accel.x, y: -this.dede.accel.y};
-            }
-            else {
 
-            }
+
+
         }
+        /*
+for (let t = 0; t < 3; t++) {
+    
+
+    let i = 0;
+    while (!this.scene.getGoodPositionScore(this.dede) && (i <= 10)) {
+        this.dede.position.x = -this.dede.speed.x;
+        this.dede.position.y = -this.dede.speed.y;
+        this.dede.accel.x = 0;
+        this.dede.accel.y = 0;
+        i++;
+    }
+   
+}*/
 
 
         let context = this.canvas.getContext("2d");
         context.clearRect(0, 0, 640, 480);
 
+
         //context.save();
         context.resetTransform();
-        
         context.translate(-this.dede.position.x + 640 / 2, -this.dede.position.y + 480 / 2);
-        
+        context.scale(2, 2);
+
+        context.drawImage(this.imgBackground, 0, 0);
+
+
+        context.resetTransform();
+        context.translate(-this.dede.position.x * 2 + 640 / 2, -this.dede.position.y * 2 + 480 / 2);
+        context.scale(2, 2);
+
         this.scene.draw(context);
         this.dede.draw(context);
 
