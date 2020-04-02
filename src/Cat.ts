2fd2df163c6924a75e91e72b61e7f2b173a66a63
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-04-02 23:17:44 +0200

    dist

--- src/Cat.ts
+++ src/Cat.ts
@@ -1,37 +1,51 @@
-type Vector2D = {x: number, y: number};
+type Vector2D = { x: number, y: number };
 
 
-const SPEEDDECREASING = 0.7;
+const SPEEDDECREASING = 0.8;
+const MOVEMENT = 10;
+const GRAVITY = 10;
+const SIZE = 64;
+const DT = 0.2;
+const JUMP = 50;
 
 export class Cat {
 
-    private position:Vector2D;
-    private speed:Vector2D;
-    private accel:Vector2D;
+    public position: Vector2D;
+    public speed: Vector2D;
+    public accel: Vector2D;
+    private img = new Image();
+    private imgJump = new Image();
 
     constructor() {
-        this.position = {x: 100, y: 200};
-        this.speed = {x: 0, y: 0};
-        this.accel = {x: 0, y: 0};
+        this.position = { x: 100, y: 1000 };
+        this.speed = { x: 0, y: 0 };
+        this.accel = { x: 0, y: 0 };
+        this.img.src = "./cat.png";
+        this.imgJump.src = "./catjump.png";
     }
 
-    left() { this.accel.x--; }
-    right() { this.accel.x++; }
-    jump() { this.accel.y=-100; }
+    left() { this.accel.x -= MOVEMENT; this.position.x--; }
+    right() { this.accel.x += MOVEMENT; this.position.x++; }
+    jump() { this.accel.y = -JUMP; }
 
-    draw(context: CanvasRenderingContext2D) {
-        this.position.x += this.speed.x;
-        this.position.y += this.speed.y;
-        this.speed.x += this.accel.x;
-        this.speed.y += this.accel.y;
+
+    live() {
+        this.position.x += this.speed.x * DT;
+        this.position.y += this.speed.y * DT;
+        this.speed.x += this.accel.x * DT;
+        this.speed.y += this.accel.y * DT;
         this.speed.x *= SPEEDDECREASING;
         this.speed.y *= SPEEDDECREASING;
         this.accel.x *= SPEEDDECREASING;
         this.accel.y += SPEEDDECREASING;
-        this.accel.y = Math.max(5, this.accel.y);
-       
-        this.position.y = Math.min(this.position.y, 300);
+        this.accel.y = Math.min(GRAVITY, this.accel.y);
 
-        context.fillRect(this.position.x, this.position.y, 32, 32);
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        if (this.accel.y < 0)
+            context.drawImage(this.img, this.position.x, this.position.y - SIZE, SIZE, SIZE);
+        else
+            context.drawImage(this.imgJump, this.position.x, this.position.y - SIZE, SIZE, SIZE);
     }
 }
--- src/Game.ts
+++ src/Game.ts
@@ -1,21 +1,53 @@
 import { Cat } from './Cat.js';
+import { Scene } from './Scene.js';
 
 
 export class Game {
     private canvas: HTMLCanvasElement;
     private dede: Cat;
+    private scene: Scene;
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
+        this.scene = new Scene();
         this.dede = new Cat();
-
     }
 
     draw() {
+
+        let previousPosition = { x: this.dede.position.x, y: this.dede.position.y };
+
+        for (let t = 0; t < 5; t++) {
+            this.dede.live();
+
+            let i = 0;
+            while(!this.scene.isGoodPosition(this.dede) ||( i <= 3)) {
+                this.dede.position.x -= this.dede.speed.x;
+                this.dede.position.y -= this.dede.speed.y;
+                i++;
+            }
+            if (i > 3) {
+                this.dede.position = previousPosition;
+                this.dede.accel = {x: -this.dede.accel.x, y: -this.dede.accel.y};
+            }
+            else {
+
+            }
+        }
+
+
         let context = this.canvas.getContext("2d");
         context.clearRect(0, 0, 640, 480);
+
+        //context.save();
+        context.resetTransform();
+        
+        context.translate(-this.dede.position.x + 640 / 2, -this.dede.position.y + 480 / 2);
+        
+        this.scene.draw(context);
         this.dede.draw(context);
 
+        // context.resetTransform();
     }
 
 
